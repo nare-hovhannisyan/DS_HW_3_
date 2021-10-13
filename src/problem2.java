de7ca1d5d676b92a740a069b2fe86cc8bd6d7a1a
Nare Hovhannisyan <nare@aikoda.com>
2021-10-13 21:34:00 +0400

    fix

--- src/problem2.java
+++ src/problem2.java
@@ -9,12 +9,12 @@ public class problem2 {
         list.addLast(3);
         list.addLast(4);
         list.addLast(5);
-        list.removeLast();
-        list.removeLast();
+        System.out.println(list.removeLast());
+        System.out.println(list.removeLast());
         list.print();
 
         // the running time of removeLast() method is linear -> O(n) because there is no way to find the element preceding the last element in constant time
         // that is why it is not included in the public interface because the running time is linear and all other methods of public interface take constant time
-        // so it is not efficient to have such method in public interface.
+        // so it is not efficient to have such method in the public interface.
     }
 }
